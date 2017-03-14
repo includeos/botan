3e768960a985762f8910f79171cbee31c402b674
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2017-03-14 14:14:27 +0100

    Updated create_bundle with description and structure

--- create_bundle.sh
+++ create_bundle.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# 1. Configure botan for includeos
+
+# 1.1. Mac
+# ./configure.py --os=includeos --cpu=x86_32 --disable-shared --minimized  --enable-modules=sha2_64,sha2_32,x509,hkdf,hmac,dev_random,tls,system_rng --cc=clang --cc-bin=/usr/local/bin/clang++  --cc-abi-flags="--target=i686-elf -D_LIBCPP_HAS_NO_THREADS=1 -D_GNU_SOURCE -nostdlib -stdlib=libc++ -nostdinc++ -L/usr/local/includeos/lib -Wl,-rpath,/usr/local/includeos/lib -msse3 -I/usr/local/includeos/api/sys -I/usr/local/includeos/include/libcxx -I/usr/local/includeos/include/newlib -v"
+# 1.2. Edit Makefile to i686-elf-ar
+
+# 2. make libs
+# 3. create_bundle.sh
+
 TARFILE="botan-includeos.tar.gz"
-tar -chzf $TARFILE --exclude "build/include/botan/internal" libbotan-2.a build/include/botan
+pushd build/include
+tar --exclude="botan/internal" -chzf ../../$TARFILE botan/ -C "../../" libbotan-2.a
+popd
 echo -e "> Created $TARFILE"
