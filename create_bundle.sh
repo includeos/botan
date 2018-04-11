128625c6a0b9edfc214ab50eef227ed32bbd3f65
fwsGonzo <fwsgonzo@hotmail.com>
2018-04-11 11:13:48 +0200

    Update bundle creation

--- create_bundle.sh
+++ create_bundle.sh
@@ -5,10 +5,12 @@
 # 1.1. Mac
 # ./configure.py --os=includeos --cpu=x86_32 --disable-shared --minimized  --enable-modules=sha2_64,sha2_32,x509,hkdf,hmac,dev_random,tls,system_rng --cc=clang --cc-bin=/usr/local/bin/clang++  --cc-abi-flags="--target=i686-elf -D_LIBCPP_HAS_NO_THREADS=1 -D_GNU_SOURCE -nostdlib -stdlib=libc++ -nostdinc++ -L/usr/local/includeos/lib -Wl,-rpath,/usr/local/includeos/lib -msse3 -I/usr/local/includeos/api/sys -I/usr/local/includeos/include/libcxx -I/usr/local/includeos/include/newlib -v"
 # 1.2. Edit Makefile to i686-elf-ar
-# 1.1 Linux
-# ./configure.py --os=includeos --cpu=x86_32 --disable-shared --minimized --enable-modules=sha2_64,sha2_32,x509,hkdf,hmac,dev_random,tls,system_rng --cc=clang --cc-abi-flags="--target=i686-elf -msse3 -D_LIBCPP_HAS_NO_THREADS=1 -D_GNU_SOURCE -nostdlib -stdlib=libc++ -nostdinc++ -L$INCLUDEOS_PREFIX/includeos/i686/lib -Wl,-rpath,$INCLUDEOS_PREFIX/includeos/i686/lib -I$INCLUDEOS_PREFIX/includeos/api -I$INCLUDEOS_PREFIX/includeos/api/posix -I$INCLUDEOS_PREFIX/includeos/i686/include/libcxx -I$INCLUDEOS_PREFIX/includeos/i686/include/newlib"
+
 ###
-### ./configure.py --os=includeos --cpu=x86_64 --disable-shared --minimized --enable-modules=sha2_64,sha2_32,x509,hkdf,hmac,dev_random,tls,system_rng --cc=clang --cc-abi-flags="--target=x86_64-elf -msse3 -D_LIBCPP_HAS_NO_THREADS=1 -D_GNU_SOURCE -nostdlib -stdlib=libc++ -nostdinc++ -L$INCLUDEOS_PREFIX/includeos/x86_64/lib -Wl,-rpath,$INCLUDEOS_PREFIX/includeos/x86_64/lib -I$INCLUDEOS_PREFIX/includeos/api -I$INCLUDEOS_PREFIX/includeos/api/posix -I$INCLUDEOS_PREFIX/includeos/x86_64/include/libcxx -I$INCLUDEOS_PREFIX/includeos/x86_64/include/newlib"
+# 1.1 Linux 32-bit
+# ./configure.py --os=includeos --cpu=x86_32 --disable-shared --minimized --enable-modules=sha2_64,sha2_32,x509,hkdf,hmac,dev_random,tls,system_rng --cc=clang --cc-abi-flags="--target=i686-pc-linux-elf -msse3 -D_LIBCPP_HAS_MUSL_LIBC -D_GNU_SOURCE -nostdlib -nostdinc++ -I$INCLUDEOS_PREFIX/includeos/api -I$INCLUDEOS_PREFIX/includeos/include -I$INCLUDEOS_PREFIX/includeos/i686/include/libcxx -I$INCLUDEOS_PREFIX/includeos/i686/include/musl"
+# 1.2 Linux 64-bit
+# ./configure.py --os=includeos --cpu=x86_64 --disable-shared --minimized --enable-modules=sha2_64,sha2_32,x509,hkdf,hmac,dev_random,tls,system_rng --cc=clang --cc-abi-flags="--target=x86_64-pc-linux-elf -msse3 -D_LIBCPP_HAS_MUSL_LIBC -D_GNU_SOURCE -nostdlib -nostdinc++ -I$INCLUDEOS_PREFIX/includeos/api -I$INCLUDEOS_PREFIX/includeos/include -I$INCLUDEOS_PREFIX/includeos/x86_64/include/libcxx -I$INCLUDEOS_PREFIX/includeos/x86_64/include/musl"
 ###
 
 # 2. make libs
